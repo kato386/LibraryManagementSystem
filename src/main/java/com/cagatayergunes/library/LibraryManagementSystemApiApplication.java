63486729c365e60858c9b1a4fb436f1473ba0e15
kato386 <cgtyysf@outlook.com>
2025-05-01 19:50:01 +0300

    feat:project initialization

---
+++ src/main/java/com/cagatayergunes/library/LibraryManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.cagatayergunes.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagatayergunes/library/LibraryManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagatayergunes.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
