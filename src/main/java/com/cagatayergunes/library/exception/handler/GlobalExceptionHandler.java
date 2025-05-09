1c3ebfde1387129589181a09200beee435f38e97
kato386 <cgtyysf@outlook.com>
2025-05-09 06:45:54 +0300

    feat: Token validation logic added.

---
+++ src/main/java/com/cagatayergunes/library/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cagatayergunes.library.exception.handler;
+
+import com.cagatayergunes.library.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exp){
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+}
